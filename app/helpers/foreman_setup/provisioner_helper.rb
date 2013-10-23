299bb87cf7349555d65bfc5a27f811e52b5b5974
Dominic Cleal <dcleal@redhat.com>
2013-10-23 20:29:42 +0100

    Move wizard steps to helper

---
+++ app/helpers/foreman_setup/provisioner_helper.rb
@@ -0,0 +1,14 @@
+module ForemanSetup
+  module ProvisionerHelper
+    def provisioner_wizard(step)
+      wizard_header(
+        step,
+         _("Pre-requisites"),
+         _("Network config"),
+         _("Foreman installer"),
+         _("Installation media"),
+         _("Completion") 
+      )
+    end
+  end
+end
