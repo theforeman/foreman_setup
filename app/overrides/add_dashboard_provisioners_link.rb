5fadbdf90fb9601d4a9da86deeb12d92a545e290
Dominic Cleal <dcleal@redhat.com>
2013-10-26 16:36:31 +0100

    Add :original in case the overridden element changes

--- app/overrides/add_dashboard_provisioners_link.rb
+++ app/overrides/add_dashboard_provisioners_link.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(:virtual_path => 'dashboard/index',
                      :name => 'add_dashboard_provisioners_link',
                      :surround_contents => 'code[erb-loud]:contains("title_actions")',
+                     :original => '35e276b7102eaa4feb8a4a84f66fd4812216fd48',
                      :text => <<EOS
 if ForemanSetup::Provisioner.any?
   <%= render_original %>
