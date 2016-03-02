782868aec0eb22e6c10beb8012cda6c653584232
Dominic Cleal <dominic@cleal.org>
2016-03-02 15:10:30 +0000

    Explicitly set limit on string columns

--- db/migrate/20131023140100_add_provisioners.rb
+++ db/migrate/20131023140100_add_provisioners.rb
@@ -3,9 +3,9 @@ class AddProvisioners < ActiveRecord::Migration
     create_table :setup_provisioners do |t|
       t.integer :host_id
       t.integer :smart_proxy_id
-      t.string :provision_interface
+      t.string :provision_interface, :limit => 255
       t.integer :subnet_id
-      t.string :timestamps
+      t.string :timestamps, :limit => 255
     end
   end
 end
