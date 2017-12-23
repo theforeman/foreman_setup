7dae3678c33532e6520fe22159514edc25a19815
Michael Moll <kvedulv@kvedulv.de>
2017-12-23 02:04:52 +0100

    stop using HttpPositionalArguments

--- test/functional/foreman_setup/provisioners_controller_test.rb
+++ test/functional/foreman_setup/provisioners_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_plugin_helper'
 
 class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
   test '#index without provisioner' do
-    get :index, {}, set_session_user
+    get :index, headers: set_session_user
     assert_redirected_to new_foreman_setup_provisioner_path
   end
 
   test '#index with provisioner' do
     FactoryBot.create :setup_provisioner
-    get :index, {}, set_session_user
+    get :index, session: set_session_user
     assert_response :success
     assert_template 'foreman_setup/provisioners/index'
   end
@@ -21,7 +21,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
     end
 
     test '#new' do
-      get :new, {}, set_session_user
+      get :new, session: set_session_user
       assert_response :success
       assert_equal @host, assigns(:host)
       assert_equal @proxy, assigns(:proxy)
@@ -29,7 +29,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
     end
 
     test '#create success' do
-      post :create, {'foreman_setup_provisioner' => {:host_id => @host.id, :smart_proxy_id => @proxy.id, :provision_interface => 'eth0'}}, set_session_user
+      post :create, params: { 'foreman_setup_provisioner' => {:host_id => @host.id, :smart_proxy_id => @proxy.id, :provision_interface => 'eth0'} }, session: set_session_user
 
       prov = ForemanSetup::Provisioner.find_by_host_id(@host.id)
       assert prov
@@ -43,7 +43,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
     end
 
     test '#create failure' do
-      post :create, {}, set_session_user
+      post :create, session: set_session_user
       assert_equal @host, assigns(:host)
       assert_equal @proxy, assigns(:proxy)
       assert_template 'foreman_setup/provisioners/_step1'
@@ -52,7 +52,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
 
   test '#step2 with new subnet' do
     prov = FactoryBot.create(:setup_provisioner, :step1)
-    get :step2, {:id => prov.id}, set_session_user
+    get :step2, params: { :id => prov.id }, session: set_session_user
     assert_response :success
     assert_template 'foreman_setup/provisioners/_step2'
 
@@ -68,7 +68,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
     prov = FactoryBot.create(:setup_provisioner, :step1)
     Facter.expects(:value).with(:fqdn).returns(prov.host.name).at_least_once
 
-    put :step2_update, {:id => prov.id, 'foreman_setup_provisioner' => {:subnet_attributes => {'name' => 'test', :network => '192.168.1.0', :mask => '255.255.255.0'}, :domain_name => prov.host.domain.name}}, set_session_user
+    put :step2_update, params: { :id => prov.id, 'foreman_setup_provisioner' => {:subnet_attributes => {'name' => 'test', :network => '192.168.1.0', :mask => '255.255.255.0'}, :domain_name => prov.host.domain.name} }, session: set_session_user
     assert_redirected_to step3_foreman_setup_provisioner_path(prov)
 
     as_admin do
@@ -94,7 +94,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
 
   test '#step3' do
     prov = FactoryBot.create(:setup_provisioner, :step2)
-    get :step3, {:id => prov.id}, set_session_user
+    get :step3, params: { :id => prov.id }, session: set_session_user
     assert_response :success
     assert_template 'foreman_setup/provisioners/_step3'
   end
@@ -108,7 +108,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
 
     ProvisioningTemplate.expects(:build_pxe_default).returns(200, 'Sucess')
 
-    get :step4, {:id => prov.id}, set_session_user
+    get :step4, params: { :id => prov.id }, session: set_session_user
     assert_response :success
     assert_template 'foreman_setup/provisioners/_step4'
     assert assigns(:medium)
@@ -131,7 +131,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
       :hostgroup_attributes => {},
       :create_medium => {:name => 'test', :path => 'http://mirror.example.com'},
     }
-    put :step4_update, {:id => prov.id, 'foreman_setup_provisioner' => attrs}, set_session_user
+    put :step4_update, params: { :id => prov.id, 'foreman_setup_provisioner' => attrs }, session: set_session_user
     assert_redirected_to step5_foreman_setup_provisioner_path(prov)
 
     as_admin do
@@ -149,7 +149,7 @@ class ForemanSetup::ProvisionersControllerTest < ActionController::TestCase
 
   test '#step5' do
     prov = FactoryBot.create(:setup_provisioner, :step2)
-    get :step5, {:id => prov.id}, set_session_user
+    get :step5, params: { :id => prov.id }, session: set_session_user
     assert_response :success
     assert_template 'foreman_setup/provisioners/_step5'
   end
