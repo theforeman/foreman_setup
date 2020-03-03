94980b7b0bfeb02a830f416fa84270dff1a29945
Michael Moll <kvedulv@kvedulv.de>
2020-03-03 16:22:59 +0100

    adjust to new Rubocop

--- app/helpers/foreman_setup/provisioner_helper.rb
+++ app/helpers/foreman_setup/provisioner_helper.rb
@@ -3,11 +3,11 @@ module ForemanSetup
     def provisioner_wizard(step)
       wizard_header(
         step,
-         _("Pre-requisites"),
-         _("Network config"),
-         _("Foreman installer"),
-         _("Installation media"),
-         _("Completion") 
+        _("Pre-requisites"),
+        _("Network config"),
+        _("Foreman installer"),
+        _("Installation media"),
+        _("Completion")
       )
     end
   end
